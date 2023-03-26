bc834a39869929d27dda1ff09591087fdda83e2d
Carla <carlaheywood24@gmail.com>
2023-03-25 17:57:05 -0700

    Secure

--- polygon/polygon/settings.py
+++ polygon/polygon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_e6ai4fn9wv$oojy+f@z1o+-kp=g4ljlsoq178vfnimzqf9c#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['44.199.196.163']
+ALLOWED_HOSTS = [' 44.211.59.253']
 
 
 
